31e0da8f42f594aea443258128ab84c388919a72
Arpit kumar <arpitk2001aa@gmail.com>
2022-07-23 10:12:17 +0000

    Mux bug number 1

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,4 +7,20 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    sel0 = 12
+    sel1 = 13
+
+    in0 = 1
+    in1 = 3
+
+    dut.inp12.value = in0;
+    dut.inp13.value = in1;
+
+    dut.sel.value = sel1;    #Output should have the value of inp13
+
+    await Timer(2, units='ns')
+
+    assert dut.out.value == in1, f"Multiplexer output is incorrect: {dut.out.value} != {dut.inp13.value}"
+
+    
+    #cocotb.log.info('##### CTB: Develop your test here ########')
